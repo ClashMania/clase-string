9d23737ea4785c88eb647d68abb548a969642a7e
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-01-11 09:09:00 +0100

    Add files via upload

---
+++ claseString/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace claseString
+{
+    internal class Program
+    {
+        private const string V = "intenzivno ";
+
+        static void Main(string[] args)
+        {
+            string recenica = "Danas radimo s klasom string!";
+
+
+            Console.WriteLine("Broj znakova u rečenici: {0}",
+                recenica.Length);
+
+            Console.WriteLine("Ispis malim slovima: {0} " + "\nIspis velikim slovima: {1}",
+                recenica.ToLower(), recenica.ToUpper());
+
+            Console.WriteLine(recenica.Substring(0, 5));
+            Console.WriteLine(recenica.Substring(recenica.Length - 5, 5));
+
+            Console.WriteLine(recenica.Replace("Danas", "Sutra"));
+
+            string[] rijeci = recenica.Split(' ');
+            for (int i = 0; i < rijeci.Length; i++)
+            {
+                Console.WriteLine(rijeci[i]);
+            }
+          
+
+            Console.WriteLine(recenica.IndexOf("radimo"));
+            Console.WriteLine(recenica.Insert(recenica.IndexOf("radimo"), "intenzivno "));
+
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
